efc01d69ff82601668108920171926674473ab5a
Eric <ewanfran@gmail.com>
2025-11-27 14:59:46 +0000

    Integrated SceneManager into main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include "SceneManager.hpp"
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Undecided");
+    window.setFramerateLimit(60);
+
+    SceneManager sceneManager;
+
+    // temporary; no scene yet
+    // add MenuScene next
+
+    sf::Clock clock;
 
     while (window.isOpen()) {
+        float dt = clock.restart().asSeconds();
+
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (sceneManager.current())
+                sceneManager.current()->handleEvent(event);
         }
 
-        window.clear(sf::Color::Black);
+        if (sceneManager.current())
+            sceneManager.current()->update(dt);
+
+        window.clear();
+        if (sceneManager.current())
+            sceneManager.current()->draw(window);
         window.display();
     }
 
